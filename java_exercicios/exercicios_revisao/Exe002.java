a61da1258eea79118e2c3917c7ab31f3bdd31d83
ViniciusOlieoeoe <viniciusoliveira12353@gmail.com>
2025-05-18 11:35:54 -0300

    exercicio 1

---
+++ java_exercicios/exercicios_revisao/Exe002.java
@@ -0,0 +1,7 @@
+public class Exe002 {
+    /*Média de Três Números
+Problema: Escreva um programa/método que receba três números e calcule a média aritmética deles.
+Entrada: 4 6 8
+Saída: 6 */
+    
+}
