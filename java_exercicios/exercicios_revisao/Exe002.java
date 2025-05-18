7d1a025458bd19592ccf32b31723126eb45f1ca3
ViniciusOlieoeoe <viniciusoliveira12353@gmail.com>
2025-05-18 11:54:42 -0300

    Média aritimética

--- java_exercicios/exercicios_revisao/Exe002.java
+++ java_exercicios/exercicios_revisao/Exe002.java
@@ -3,5 +3,12 @@ public class Exe002 {
 Problema: Escreva um programa/método que receba três números e calcule a média aritmética deles.
 Entrada: 4 6 8
 Saída: 6 */
-    
+    public static void main(String []args){
+        int entrada1 = 4;
+        int entrada2 = 6;
+        int entrada3 = 8;
+        int media  = (entrada1+entrada2+entrada3)/3;
+        System.out.println("A média de "+entrada1+" + "+entrada2+" + "+entrada3+" é: "+media);
+    }
+
 }
