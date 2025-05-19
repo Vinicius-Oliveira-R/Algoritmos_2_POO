cadf2d761a4467b08f963d4b1543aa66debe7c8a
ViniciusOlieoeoe <viniciusoliveira12353@gmail.com>
2025-05-19 10:05:48 -0300

    palindromo

---
+++ java_exercicios/exercicios_revisao/Exe005.java
@@ -0,0 +1,12 @@
+public class Exe005 {
+    public static void main(String[] args) {
+        /*
+         * Palíndromo
+         * Problema: Verifique se uma palavra é um palíndromo.
+         * Entrada: radar
+         * Saída: Sim
+         */
+        String palavra = "radar";
+        
+    }
+}
