5d39e165a455a2d1ac67928e22993e137e400259
ViniciusOlieoeoe <viniciusoliveira12353@gmail.com>
2025-05-20 14:49:44 -0300

    Ordenar Lista

---
+++ java_exercicios/exercicios_revisao/Exe007.java
@@ -0,0 +1,24 @@
+public class Exe007 {
+    public static void main(String[] args) {
+        /*
+         * Ordenação de Três Números
+         * Problema: Ordene três números em ordem crescente.
+         * Entrada: 3 1 2
+         * Saída: 1 2 3
+         */
+        int[] entrada = {3,2,1};
+        int aux;
+        for (int i = 0; i < entrada.length; i++) {
+            for (int j = 0; j < entrada.length-1; j++) {
+                if(entrada[j]>entrada[j+1]){
+                    aux = entrada[j];
+                    entrada[j]=entrada[j+1];
+                    entrada[j+1] = aux;
+                }
+            }
+        }
+        for (int i = 0; i < entrada.length; i++) {
+            System.out.println(entrada[i]);
+        }
+    }
+}
